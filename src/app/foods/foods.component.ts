0db1b80c7eebece492a300ea474b66d16a4e20c4
Piotr Wojenski <pwojenskip@gmail.com>
2024-05-18 21:31:47 +0200

    Update foods.component.html to display item names, IDs, calories per 100g, and photos

--- src/app/foods/foods.component.ts
+++ src/app/foods/foods.component.ts
@@ -2,18 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../food.service';
 import { Observable, map } from 'rxjs';
 import { AsyncPipe, JsonPipe, NgFor } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-foods',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, NgFor],
+  imports: [AsyncPipe, JsonPipe, NgFor, MatTableModule],
   templateUrl: './foods.component.html',
-  styleUrl: './foods.component.scss',
+  styleUrls: ['./foods.component.scss'],
 })
 export class FoodsComponent implements OnInit {
   foodResponse$ = this.foodService.getFood();
   food$ = this.foodResponse$.pipe(map((response) => response.data));
   length$ = this.foodResponse$.pipe(map((response) => response.length));
+
+  displayedColumns = ['id', 'name', 'weight', 'symbol']; // Define columns to display in the table
 
   constructor(private foodService: FoodService) {}
 
